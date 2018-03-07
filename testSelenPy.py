b13ff954cdae6c34b444111c15b6038080ca08bd
Manuel Martinez <mgm264@cornell.edu>
2018-03-06 22:03:36 -0800

    updating procedure for headless mode

--- testSelenPy.py
+++ testSelenPy.py
@@ -1,19 +1,50 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("https://mondotees.com")
-#assert "mondotees" in driver.title
-icon = driver.find_element_by_css_selector('.icon-search.hdr-search').click()
-#elem.clear()
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+q = "batman-1-6-scale-figure"
+#For headless add arg : chrome_options = chrome_options
+driver = webdriver.Chrome(chrome_options = chrome_options)
+driver.get("https://mondotees.com/search?q=")
+
 searchBar = driver.find_element_by_name("q")
 searchBar.clear()
 
-searchBar.send_keys("batman-1-6-scale-figure")
+searchBar.send_keys(q)
 searchBar.send_keys(Keys.RETURN)
 
+lists= driver.find_elements_by_class_name("search-result-title")
+print ("Found"  + str(len(lists)) + "searches:")
+lists[0].click();
+
+addToCart = driver.find_element_by_name("add").click()
+time.sleep(.5)
+driver.get("https://mondotees.com/cart")
+
+checkOut = driver.find_element_by_name("checkout")
+checkOut.click()
+#email = driver.find_element_by_name("checkout[email]")
+#email.clear()
+#email.send_keys("mgm264@cornell.edu")
+
+
+print('Success search')
+driver.get_screenshot_as_file("capture.png")
+time.sleep(5) # Let the user actually see something!
+
 assert "No results found." not in driver.page_source
-#time.sleep(5) # Let the user actually see something!
+
 
 driver.close()
+
+
+#assert "mondotees" in driver.title
+#icon = driver.find_element_by_css_selector('.icon-search.hdr-search').click()
+#elem.clear()
+
+#
